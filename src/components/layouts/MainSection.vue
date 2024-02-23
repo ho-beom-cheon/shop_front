c145070db300b2d30a1aed13ee8370eb6629985c
ho-beom-cheon <cjs414ghqja@gmail.com>
2024-02-23 17:20:33 +0900

    shop_front-1 #2 기본 화면 구성 * main.js import 추가 - router - store - vue-sweetalert2 - sweetalert2.min.css

--- src/components/layouts/MainSection.vue
+++ src/components/layouts/MainSection.vue
@@ -1,44 +1,31 @@
 <template>
   <section class="category__box">
     <div>a</div>
-<!--    <ProductList-->
-<!--        category="생활가전"-->
-<!--        class="main" />-->
   </section>
   <section class="category__box">
-    <div>b</div>
-<!--    <ProductLis<div>a</div>t-->
-<!--        category="계절가전"-->
-<!--        class="main" />-->
+    <btton @click="click">click</btton>
   </section>
   <section class="category__box">
     <div>c</div>
-<!--    <ProductList-->
-<!--        category="디지털"-->
-<!--        class="main" />-->
   </section>
 </template>
 
 <script>
 
 export default {
-  // components: {
-  //   ProductList,
-  // },
-  // mounted() {
-  //   this.readAllProducts()
-  // },
-  // methods: {
-  //   ...mapActions('product', ['readAllProducts'])
-  // }
-
+  components: {
+  },
+  setup() {
+    const click = () => {
+      console.log('clicked')
+    }
+    return {
+      click,
+    }
+  }
 }
 </script>
 
-<!--<style lang="scss" scoped>-->
-<!--.category__box {-->
-<!--  margin-top: 5rem;-->
-<!--}-->
 <style>
 @media screen and (max-width: 380px) {
   .category__box {
