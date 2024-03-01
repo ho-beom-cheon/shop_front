de9c06cbc41d82db08d9d95655d403f72c49d9d2
ho-beom-cheon <cjs414ghqja@gmail.com>
2024-03-01 23:29:56 +0900

    shop_front-3 #2 Login 화면 레이아웃
    - scss 파일 추가
    - Login.vue 추가
    - .eslintrc.json 규칙 추가

---
+++ src/routes/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <MainSection />
+  <div class="container">
+    <form class="form" @submit.prevent="submitForm">
+      <div class="form-group">
+        <label for="email" />
+        <input
+          id="email"
+          v-model.trim="state.email"
+          placeholder="Email"
+          type="email"
+          class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="password" />
+        <input
+          id="password"
+          v-model.trim="state.password"
+          placeholder="Password"
+          type="password"
+          class="form-control">
+      </div>
+      <div class="form-group">
+        <button
+          class="btn btn-primary"
+          type="submit"
+          :disabled="!validated">
+          로그인
+        </button>
+        <button
+          type="button"
+          class="btn btn-light"
+          @click="$router.push('/signup')">
+          회원가입
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+export default {
+  setup() {
+    const state = reactive({
+      email: '',
+      password: '',
+    })
+
+    return {
+          state
+    }
+  },
+}
+</script>
